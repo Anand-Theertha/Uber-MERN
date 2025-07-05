b4c666383e4b690c7fe49c4d1247483d38d94b3f
Anand-Theertha <anandtheerthav3@gmail.com>
2025-07-05 19:24:22 +0000

    User Profile

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectToDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
@@ -9,6 +10,7 @@ const userRoutes = require("./routes/user.routes");
 connectToDB();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
