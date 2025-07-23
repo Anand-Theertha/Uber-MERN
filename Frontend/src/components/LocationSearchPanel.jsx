5d5f27e489e50c8a7b3cac91f08b1be26188cc35
Anand-Theertha <anandtheerthav3@gmail.com>
2025-07-23 15:40:56 +0000

    Location Search Panel Initial commit

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LocationSearchPanel = () => {
-  return <div>LocationSearchPanel</div>;
+  return (
+    <div>
+      <div className="flex justify-start my-4 items-center gap-4">
+        <h2 className="bg-[#EEE] rounded-full h-8 w-12 flex items-center justify-center">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="font-medium">
+          Random addressRandom address Random address Random address
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default LocationSearchPanel;
